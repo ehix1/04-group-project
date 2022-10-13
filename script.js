a607a379558995aea8ca373c944cf74b9099ce89
Deshawn Reid <deshawnreid2021@gmail.com>
2022-10-13 15:04:32 -0400

    back of card img

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const cardContainer = document.querySelector(".card-container");
 
 for (let i = 0; i < 8; i++) {
   const newCard = document.createElement("div");
-  cardContainer.append(newCard);
   newCard.classList.add("card");
+  newCard.style.backgroundImage = "url(/assets/Tarot-back.webp)";
+  cardContainer.append(newCard);
+  
+  
 }
