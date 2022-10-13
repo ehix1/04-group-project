ca6e211d02b5365ca65e59550f1a11e1aedf6387
Deshawn Reid <deshawnreid2021@gmail.com>
2022-10-13 16:03:50 -0400

    5th commit

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 "use strict";
 const cardContainer = document.querySelector(".card-container");
 
+
 for (let i = 0; i < 8; i++) {
   const newCard = document.createElement("div");
+  const backCard = document.createElement("div");
+  const faceCard = document.createElement("div");
+
   newCard.classList.add("card");
-  newCard.style.backgroundImage = "url(/assets/Tarot-back.webp)";
+  backCard.classList.add("back-card");
+  faceCard.classList.add("face-card");
+  backCard.style.backgroundImage = "url(/assets/Tarot-back.webp)";
+
   cardContainer.append(newCard);
+  cardContainer.append(backCard);
+  cardContainer.append(faceCard);
+ 
   
   
 }
+
+cardContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("back-card")) {
+  
+  }
+});
