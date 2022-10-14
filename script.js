b0b0921e37b7129f076657d488848c44ac36f25f
Erica Hickson <ericahickson@Ericas-MacBook-Pro.local>
2022-10-14 09:21:15 -0400

    update

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
 "use strict";
-const cardContainer = document.querySelector(".card-container");
 
-for (let i = 0; i < 8; i++) {
-  const newCard = document.createElement("div");
-  newCard.classList.add("card");
-  newCard.style.backgroundImage = "url(/assets/Tarot-back.webp)";
-  cardContainer.append(newCard);
-  
-  
-}
+const arrayOfCards = [
+  {
+    name: "death",
+    img: "/assets/death.jpeg",
+  },
+  {
+    name: "death",
+    img: "/assets/death.jpeg",
+  },
+  {
+    name: "magician",
+    img: "/assets/magician.jpeg",
+  },
+  {
+    name: "magician",
+    img: "/assets/magician.jpeg",
+  },
+  {
+    name: "moon",
+    img: "/assets/moon.jpeg",
+  },
+  {
+    name: "moon",
+    img: "/assets/moon.jpeg",
+  },
+  {
+    name: "sun",
+    img: "/assets/sun.jpeg",
+  },
+  {
+    name: "sun",
+    img: "/assets/sun.jpeg",
+  },
+];
+
+// const cardContainer = document.querySelector(".card-container");
+// for (let i = 0; i < 8; i++) {
+//   const newCard = document.createElement("div");
+//   const backCard = document.createElement("div");
+//   const faceCard = document.createElement("div");
+//   newCard.classList.add("card");
+//   backCard.classList.add("back-card");
+//   faceCard.classList.add("face-card");
+//   backCard.style.backgroundImage = "url(/assets/Tarot-back.webp)";
+//   cardContainer.append(newCard);
+//   card.append(backCard);
+//   card.append(faceCard);
+// }
+// cardContainer.addEventListener("click", (e) => {
+//   if (e.target.classList.contains("back-card")) {
+//   }
+// });
