446541597a0034cc14e21150b4415c822be12f1b
Deshawn Reid <deshawnreid2021@gmail.com>
2022-10-14 11:23:28 -0400

    css commit

--- script.js
+++ script.js
@@ -1,54 +1,79 @@
-"use strict";
 
+
+//array of objects
 const arrayOfCards = [
   {
     name: "death",
-    img: "/assets/death.jpeg",
+    img: "url(/assets/death.jpeg)",
   },
   {
     name: "death",
-    img: "/assets/death.jpeg",
+    img: "url(/assets/death.jpeg)",
   },
   {
     name: "magician",
-    img: "/assets/magician.jpeg",
+    img: "url(/assets/magician.jpeg)",
   },
   {
     name: "magician",
-    img: "/assets/magician.jpeg",
+    img: "url(/assets/magician.jpeg)",
   },
   {
     name: "moon",
-    img: "/assets/moon.jpeg",
+    img: "url(/assets/moon.jpeg)",
   },
   {
     name: "moon",
-    img: "/assets/moon.jpeg",
+    img: "url(/assets/moon.jpeg)",
   },
   {
     name: "sun",
-    img: "/assets/sun.jpeg",
+    img: "url(/assets/sun.jpeg)",
   },
   {
     name: "sun",
-    img: "/assets/sun.jpeg",
+    img: "url(/assets/sun.jpeg)",
   },
 ];
 
-// const cardContainer = document.querySelector(".card-container");
-// for (let i = 0; i < 8; i++) {
-//   const newCard = document.createElement("div");
-//   const backCard = document.createElement("div");
-//   const faceCard = document.createElement("div");
-//   newCard.classList.add("card");
-//   backCard.classList.add("back-card");
-//   faceCard.classList.add("face-card");
-//   backCard.style.backgroundImage = "url(/assets/Tarot-back.webp)";
-//   cardContainer.append(newCard);
-//   card.append(backCard);
-//   card.append(faceCard);
-// }
-// cardContainer.addEventListener("click", (e) => {
-//   if (e.target.classList.contains("back-card")) {
-//   }
-// });
+
+
+
+//card container
+const cardContainer = document.querySelector(".card-container");
+for (let i = 0; i < arrayOfCards.length; i++) {
+  const newCard = document.createElement("div");
+  const backCard = document.createElement("div");
+  const faceCard = document.createElement("div");
+  newCard.classList.add("card");
+  newCard.classList.add("flip");
+  backCard.classList.add("back-card");
+  faceCard.classList.add("face-card");
+backCard.style.backgroundImage = "url(/assets/Tarot-back.webp)";
+faceCard.style.backgroundImage = arrayOfCards[i].img;
+  cardContainer.append(newCard);
+  newCard.append(backCard);
+  newCard.append(faceCard);
+  console.log(newCard);
+}
+
+//card shuffle
+const cardShuffle = () => {
+  cardContainer.forEach(card => {
+    let randomPosition = Math.floor(Math.random() * 8);
+  
+  });
+}
+
+// (function shuffle() {
+//     cards.forEach(card => {
+//         let randomPos = Math.floor(Math.random() * 12);
+//         card.style.order = randomPos;
+//     });
+// })();
+
+cardContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("back-card")) {
+  }
+});
+
